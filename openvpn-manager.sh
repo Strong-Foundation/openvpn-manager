07f051f523f919cd232fa215e4815905e89c42ac
Prajwal Koirala <179532304+Prajwal-Koirala-DEV@users.noreply.github.com>
2025-01-17 13:35:47 -0500

    update

--- openvpn-manager.sh
+++ openvpn-manager.sh
@@ -81,6 +81,31 @@ function installing_system_requirements() {
 # Call the function to check for system requirements and install necessary packages if needed
 installing_system_requirements
 
+# Checking For Virtualization
+function virt_check() {
+  # This code checks if the system is running in a supported virtualization.
+  # It returns the name of the virtualization if it is supported, or "none" if
+  # it is not supported. This code is used to check if the system is running in
+  # a virtual machine, and if so, if it is running in a supported virtualization.
+  # systemd-detect-virt is a utility that detects the type of virtualization
+  # that the system is running on. It returns a string that indicates the name
+  # of the virtualization, such as "kvm" or "vmware".
+  CURRENT_SYSTEM_VIRTUALIZATION=$(systemd-detect-virt)
+  # This case statement checks if the virtualization that the system is running
+  # on is supported. If it is not supported, the script will print an error
+  # message and exit.
+  case ${CURRENT_SYSTEM_VIRTUALIZATION} in
+  "kvm" | "none" | "qemu" | "lxc" | "microsoft" | "vmware" | "xen" | "amazon" | "docker") ;;
+  *)
+    echo "Error: the ${CURRENT_SYSTEM_VIRTUALIZATION} virtualization is currently not supported. Please stay tuned for future updates."
+    exit
+    ;;
+  esac
+}
+
+# Call the virt_check function to check for supported virtualization.
+virt_check
+
 # The following function checks if the current init system is one of the allowed options.
 function check_current_init_system() {
   # Get the current init system by checking the process name of PID 1.
