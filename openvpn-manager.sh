c197b71abc6dd6cc8e48e00e3a5611fd97595bbc
Strong Foundation <179532304+Strong-Foundation@users.noreply.github.com>
2025-04-03 11:13:52 -0400

    updated some functions

--- openvpn-manager.sh
+++ openvpn-manager.sh
@@ -36,30 +36,41 @@
 
 # Note: This script is provided 'as is', without warranty of any kind. The user is responsible for understanding the operations and risks involved.
 
-# Check if the script is running as root
+# Define a function to check if the script is being run with root privileges
 function check_root() {
-  if [ "$(id -u)" -ne 0 ]; then
+  # Compare the user ID of the current user to 0, which is the ID for root
+  if [ "$(id -u)" != "0" ]; then
+    # If the user ID is not 0 (i.e., not root), print an error message
     echo "Error: This script must be run as root."
-    exit
+    # Exit the script with a status code of 1, indicating an error
+    exit 1 # Exit the script with an error code.
   fi
 }
 
-# Call the function to check root privileges
+# Call the check_root function to verify that the script is executed with root privileges
 check_root
 
-# Function to gather current system details
+# Define a function to gather and store system-related information
 function system_information() {
-  # This function fetches the ID, version, and major version of the current system
+  # Check if the /etc/os-release file exists, which contains information about the OS
   if [ -f /etc/os-release ]; then
-    # If /etc/os-release file is present, source it to obtain system details
-    # shellcheck source=/dev/null
+    # If the /etc/os-release file is present, source it to load system details into environment variables
+    # shellcheck source=/dev/null  # Instructs shellcheck to ignore warnings about sourcing files
     source /etc/os-release
-    CURRENT_DISTRO=${ID}                 # CURRENT_DISTRO holds the system's ID
-    CURRENT_DISTRO_VERSION=${VERSION_ID} # CURRENT_DISTRO_VERSION holds the system's VERSION_ID
+    # Set the CURRENT_DISTRO variable to the system's distribution ID (e.g., 'ubuntu', 'debian')
+    CURRENT_DISTRO=${ID}
+    # Set the CURRENT_DISTRO_VERSION variable to the system's version ID (e.g., '20.04' for Ubuntu 20.04)
+    CURRENT_DISTRO_VERSION=${VERSION_ID}
+    # Set the CURRENT_SYSTEM_ARCHITECTURE variable to the system's architecture (e.g., 'x86_64', 'arm64')
+    CURRENT_SYSTEM_ARCHITECTURE=$(uname -m)
+  else
+    # If the /etc/os-release file is not present, show an error message and exit
+    echo "Error: /etc/os-release file not found. Unable to gather system information."
+    exit 1 # Exit the script with a non-zero status to indicate an error
   fi
 }
 
-# Invoke the system_information function
+# Call the system_information function to gather the system details
 system_information
 
 # Function to install either resolvconf or openresolv, depending on the distribution.
